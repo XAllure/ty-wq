a61d42cad7dfde2221fc39007ea60b1ff247c316
XAllure <66450680+XAllure@users.noreply.github.com>
2021-04-30 18:42:43 +0800

    1.添加Channel存储工具类 2.封装消息返回工具类 3.添加登录验证，存储用户i与 netty 服务器所对应的 Channel 与 token

---
+++ ty-wq-core/src/main/java/com/ty/wq/pojo/vo/netty/MsgVo.java
@@ -0,0 +1,17 @@
+package com.ty.wq.pojo.vo.netty;
+
+import lombok.Data;
+
+/**
+ * @author Administrator
+ */
+@Data
+public class MsgVo {
+
+    private String type;
+
+    private String token;
+
+    private Object data;
+
+}
