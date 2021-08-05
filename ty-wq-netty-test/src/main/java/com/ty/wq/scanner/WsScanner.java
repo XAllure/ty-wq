e693e7b9d8572fe2e5f953ae9b4b8b3aaea26adb
XAllure <66450680+XAllure@users.noreply.github.com>
2021-08-05 13:48:33 +0800

    修改

--- ty-wq-netty-test/src/main/java/com/ty/wq/scanner/WsScanner.java
+++ ty-wq-netty-test/src/main/java/com/ty/wq/scanner/WsScanner.java
@@ -26,37 +26,16 @@ public class WsScanner implements Runnable {
         //noinspection InfiniteLoopStatement
         while (true) {
             try {
-                System.out.println("链接为空发送 websocket 消息，否则发送 http 消息");
-                System.out.println("请输入消息类型");
-                String type = scanner.nextLine();
                 System.out.println("请输入消息内容");
                 String data = scanner.nextLine();
                 System.out.println("请输入链接后缀");
                 String url = scanner.nextLine();
-                // 如果链接不为空 则发送http消息
                 if (StringUtils.isNotBlank(url)) {
-                    if (StringUtils.isBlank(data)) {
-                        data = "empty:" + true;
-                    }
                     data = "{" + data + "}";
-                    // data 需要为空，或是格式为 username:"墨染",userNick:"红尘" 才行，这可以对应每一个Controller 的 @RequestBody 后的vo实体类， 单个字符加在链接后面
+                    // data 需要为空，或是格式为 username:"墨染",userNick:"红尘" 才行，这可以对应每一个Controller 的 @RequestBody 后的vo实体类
                     JSONObject json = JSON.parseObject(data);
                     String res = HttpUtils.post(WebSocketClient.URL.concat(url), WebSocketClient.token, json);
                     log.info("发送信息返回：" + res);
-                }
-                // 否则发送 websocket 消息
-                else {
-                    if (StringUtils.isBlank(type)) {
-                        // log.info("请输入消息类型！");
-                        continue;
-                    }
-                    MsgVo msgVo = new MsgVo();
-                    msgVo.setType(type);
-                    msgVo.setToken(WebSocketClient.token);
-                    data = "{" + data + "}";
-                    JSONObject json = JSON.parseObject(data);
-                    msgVo.setData(json);
-                    MsgUtils.writeJson(WebSocketClient.channel, Message.success(msgVo));
                 }
             } catch (Exception e) {
                 log.info("出错了");
