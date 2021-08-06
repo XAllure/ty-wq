166e7ed3be0cca926d397fae395dfb43d8914fb0
XAllure <66450680+XAllure@users.noreply.github.com>
2021-08-06 18:39:58 +0800

    修改

--- ty-wq-manager/src/main/java/com/ty/wq/config/ShiroConfig.java
+++ ty-wq-manager/src/main/java/com/ty/wq/config/ShiroConfig.java
@@ -9,14 +9,10 @@ import org.apache.shiro.mgt.SessionsSecurityManager;
 import org.apache.shiro.session.mgt.SessionManager;
 import org.apache.shiro.session.mgt.eis.MemorySessionDAO;
 import org.apache.shiro.session.mgt.eis.SessionDAO;
-import org.apache.shiro.spring.LifecycleBeanPostProcessor;
-import org.apache.shiro.spring.security.interceptor.AuthorizationAttributeSourceAdvisor;
 import org.apache.shiro.spring.web.ShiroFilterFactoryBean;
 import org.apache.shiro.web.mgt.DefaultWebSecurityManager;
-import org.springframework.aop.framework.autoproxy.DefaultAdvisorAutoProxyCreator;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.DependsOn;
 
 import javax.servlet.Filter;
 import java.util.LinkedHashMap;
@@ -106,8 +102,6 @@ public class ShiroConfig {
         Map<String, Filter> filterMap = new LinkedHashMap<>();
         //检测是否登录等
         filterMap.put("authc", new ShiroAuthorizationFilter());
-        //检测是否有链接操作权限
-        filterMap.put("perms", new ShiroPermissionsFilter());
         //配置自定义登出 覆盖 logout 之前默认的LogoutFilter
         filterMap.put("logout", new ShiroLogoutFilter());
         shiroFilter.setFilters(filterMap);
